42d16b624ced0414a223d58ea004913480737980
Pedro Marcelo <pedrocarvalhomarcelo@hotmail.com>
2018-10-19 15:09:32 -0300

    adicionada um item em disastre.cs e criada a classe login.cs

--- ItsEarth/ItsEarth/ItsEarth/Models/Disasters.cs
+++ ItsEarth/ItsEarth/ItsEarth/Models/Disasters.cs
@@ -10,5 +10,6 @@ namespace ItsEarth.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public Bag BagPattern { get; set; }
+        public List<string> midia { get; set; }
     }
 }
---
+++ ItsEarth/ItsEarth/ItsEarth/ViewModels/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsEarth.ViewModels
+{
+    class Login {
+
+
+    }
+}
