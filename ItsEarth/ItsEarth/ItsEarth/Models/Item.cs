63ed5ea9bba3eab0dca7a239d7cfcfead706809e
RafaBessa <rafablou@hotmail.com>
2018-10-20 13:58:30 -0300

    selecting item type

--- ItsEarth/ItsEarth/ItsEarth/Models/Item.cs
+++ ItsEarth/ItsEarth/ItsEarth/Models/Item.cs
@@ -2,6 +2,21 @@
 
 namespace ItsEarth.Models
 {
+    public enum ItemClasstype
+    {
+        Health,
+        Food,
+        Fire,
+        Construction,
+        Clothing
+    }
+
+    public class ItemClass
+    {
+        public ItemClasstype Class { get; set; }
+        public string Logo { get; set; }    
+    }
+
     public class Item
     {
         public string Id { get; set; }
@@ -9,5 +24,6 @@ namespace ItsEarth.Models
         //  public string Text { get; set; }
         public string Description { get; set; }
         public string Logo {get;set;}
+        public ItemClasstype ItemClasstype { get; set; }
     }
 }
--- ItsEarth/ItsEarth/ItsEarth/Views/BagCreationPage.xaml.cs
+++ ItsEarth/ItsEarth/ItsEarth/Views/BagCreationPage.xaml.cs
@@ -14,10 +14,31 @@ namespace ItsEarth.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BagCreationPage : ContentPage
 	{
-		public BagCreationPage ()
+        public static List<ItemClass> Categories = new List<ItemClass>
+        {
+            new ItemClass {Class = ItemClasstype.Health ,Logo = "logotst1.jpg"},
+            new ItemClass {Class = ItemClasstype.Clothing, Logo = "logotst2.png"},
+            new ItemClass {Class = ItemClasstype.Fire, Logo = "logotst3.png"},
+            new ItemClass {Class = ItemClasstype.Construction, Logo = "firstaid1.png"},
+            new ItemClass {Class = ItemClasstype.Food, Logo = "iconalert.jpg"}
+
+        };
+
+
+        public List<Item> Items = new List<Item> {
+            new Item {ItemClasstype = ItemClasstype.Health, Logo ="logotst1.jpg" },
+            new Item {ItemClasstype = ItemClasstype.Construction, Logo ="logotst1.jpg" },
+            new Item {ItemClasstype = ItemClasstype.Fire, Logo ="logotst1.jpg" },
+            new Item {ItemClasstype = ItemClasstype.Food, Logo ="logotst1.jpg" },
+            new Item {ItemClasstype = ItemClasstype.Clothing, Logo ="logotst1.jpg" },
+        };
+        public BagCreationPage ()
 		{
 
             InitializeComponent ();
+            FillStackLayout(ItemClassSelector, (from x in Categories select x.Logo).ToList());
+            ConfigItemSelector();
+            HealthStack.Children.Add(new Image { Source = "firstaid1.png"  });
             HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
             HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
             HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
@@ -26,12 +47,47 @@ namespace ItsEarth.Views
             HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
             HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
             HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
-            HealthStack.Children.Add(new Image { Source = "firstaid1.png" });
-
-
-
-
-
         }
-    }
+
+        private void ConfigItemSelector()
+        {
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            //			tapGestureRecognizer.NumberOfTapsRequired = 2; // double-tap
+            tapGestureRecognizer.Tapped += ClassSelectorTap;
+            foreach (Image x in ItemClassSelector.Children)
+            {
+                x.GestureRecognizers.Add(tapGestureRecognizer); 
+
+
+            }
+        }
+
+       
+
+        private void ClassSelctorAction(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FillStackLayout(StackLayout x, List<string> image)
+        {
+            x.Children.Clear();
+            foreach(string Y in image)
+            {
+                x.Children.Add(new Image { Source = Y });
+            }
+        }
+
+        void ClassSelectorTap(object sender, EventArgs args)
+        {
+            
+            string MyObj;
+            string mb = (sender as Image).Source.ToString();
+            System.Diagnostics.Debug.WriteLine(mb);
+            mb = mb.Split(' ')[1];
+            ItemClass cat = Categories.Find(x => x.Logo == mb);
+            tst.Text = mb;
+            FillStackLayout(ItemList, (from x in Items where x.ItemClasstype == cat.Class select x.Logo).ToList());
+        }
+        }
 }
